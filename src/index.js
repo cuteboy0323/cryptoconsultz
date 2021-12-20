d25c5f5d99b672755360733c9a576665ccbf18d9
✨Starbinna✨ <starbinna1004@gmail.com>
2021-12-21 06:52:27 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// ** Import Providers
+import MaterialThemeProvider from "./providers/theme";
+
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <MaterialThemeProvider>
+            <App />
+        </MaterialThemeProvider>
     </React.StrictMode>,
     document.getElementById('app-root')
 );
