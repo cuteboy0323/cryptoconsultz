1cd5f0d6384bb4fa7e8f6b120102f17e3b964f9d
✨Starbinna✨ <starbinna1004@gmail.com>
2021-12-21 06:52:20 +0900

    Update BaseLayout.js

--- src/components/BaseLayout.js
+++ src/components/BaseLayout.js
@@ -1,28 +1,18 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
-import Spinner from "./Spinner";
-import useStyles from "../assets/styles";
-
-import clsx from "clsx";
-import useMediaQuery from "@mui/material/useMediaQuery";
+import Container from "./Container";
 
 const BaseLayout = ({ children }) => {
-    const classes = useStyles();
-    const isMobile = useMediaQuery("(max-width:600px)");
     return (
         <>
             <Header />
-            <main
-                className={clsx(classes.content, {
-                    [classes.mobileContent]: isMobile,
-                })}
-            >
+            <Container>
                 {children}
-            </main>
+            </Container>
             <Footer />
-            <Spinner isLoading={false} />
         </>
-    );
-};
+    )
+}
+
 export default BaseLayout;
