ab36e1edd633f1b76f7b4be431a01f1025745d65
✨Starbinna✨ <starbinna1004@gmail.com>
2021-12-21 06:52:31 +0900

    Create theme.js

---
+++ src/providers/theme.js
@@ -0,0 +1,52 @@
+import React from "react";
+
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
+
+import "../assets/scss/index.scss";
+
+// ** Declare Theme Provider
+const MaterialThemeProvider = ({ children }) => {
+
+    const themeConfig = {
+        palette: {
+            mode: "dark",
+            primary: {
+                main: "#5ca7c3",
+            },
+            secondary: {
+                main: "#455ee7"
+            },
+            background: {
+                default: "#121227",
+                paper: "#181837"
+            },
+            common: {
+                black: "#121227",
+                white: "#26264e"
+            }
+        },
+        typography: {
+            fontFamily: "'DM Sans', sans-serif",
+            fontSize: 14,
+        },
+        components: {
+            MuiIconButton: {
+                styleOverrides: {
+                    root: {
+                        borderRadius: 4
+                    }
+                }
+            },
+        }
+    };
+
+    const theme = createTheme(themeConfig);
+    return (
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            {children}
+        </ThemeProvider>
+    );
+};
+export default MaterialThemeProvider;
