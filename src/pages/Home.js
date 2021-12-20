0666a313b125d6ccdcb863c67353a7ba95fdad32
✨Starbinna✨ <starbinna1004@gmail.com>
2021-12-21 06:52:28 +0900

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,32 @@
 import React from "react";
 
+import Box from "@mui/material/Box";
+import Container from '@mui/material/Container';
+import Typography from "@mui/material/Typography";
+
+import useStyles from "../assets/styles";
+
+import Logo from "../assets/img/logo.png";
+
 const Home = () => {
-    return(
-        <div>Home</div>
+    const classes = useStyles();
+
+    return (
+        <Box className={classes.home}>
+            <Box component="section" className="hero">
+                <Container>
+                    <Box className="logo-banner">
+                        <Box component="img" src={Logo} alt="logo" />
+                        <Typography variant="h4" className="title">
+                            CryptoConsultz
+                        </Typography>
+                        <Typography variant="h6" className="sub-title">
+                            Blockchain & Digital Currency Consulting Services
+                        </Typography>
+                    </Box>
+                </Container>
+            </Box>
+        </Box>
     )
 }
 
