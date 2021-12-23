c4d1737d5796358d4fefcefbab81e213cb7da56d
✨Starbinna✨ <starbinna1004@gmail.com>
2021-12-24 02:50:28 +0900

    Update Router.js

--- src/Router.js
+++ src/Router.js
@@ -13,6 +13,14 @@ import {
 
 // ** Pages ** //
 const Home = lazy(() => import("./pages/Home"));
+const Services = lazy(() => import("./pages/Services"));
+const SessionPackages = lazy(() => import("./pages/SessionPackages"));
+const Team = lazy(() => import("./pages/Team"));
+const Contact = lazy(() => import("./pages/Contact"));
+const CustomerService = lazy(() => import("./pages/CustomerService"));
+const Schedule = lazy(() => import("./pages/Schedule"));
+const Meeting = lazy(() => import("./pages/Meeting"));
+const Learning = lazy(() => import("./pages/Learning"));
 
 const AppRouter = () => {
     return (
@@ -21,6 +29,14 @@ const AppRouter = () => {
                 <BaseLayout>
                     <Routes>
                         <Route path="/" exact element={<Home />} />
+                        <Route path="/services" exact element={<Services />} />
+                        <Route path="/session-packages-and-pricing" exact element={<SessionPackages />} />
+                        <Route path="/team" exact element={<Team />} />
+                        <Route path="/contact" exact element={<Contact />} />
+                        <Route path="/schedule-and-pay" exact element={<Schedule />} />
+                        <Route path="/customer-service" exact element={<CustomerService />} />
+                        <Route path="/meeting-detail" exact element={<Meeting />} />
+                        <Route path="/learning" exact element={<Learning />} />
                     </Routes>
                 </BaseLayout>
             </Suspense>
