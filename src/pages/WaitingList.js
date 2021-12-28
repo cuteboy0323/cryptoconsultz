9ec3f21062ff0dc200f732962b36399a7db3e3a1
✨Starbinna✨ <starbinna1004@gmail.com>
2021-12-28 22:50:48 +0900

    Update WaitingList.js

--- src/pages/WaitingList.js
+++ src/pages/WaitingList.js
@@ -6,6 +6,16 @@ import Typography from "@mui/material/Typography";
 import Breadcrumbs from '@mui/material/Breadcrumbs';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+import FormGroup from '@mui/material/FormGroup';
+import Checkbox from '@mui/material/Checkbox';
+import Button from '@mui/material/Button';
 
 import useStyles from "../assets/styles";
 
@@ -13,7 +23,7 @@ const Services = () => {
     const classes = useStyles();
 
     return (
-        <Box className={classes.sessionPackages}>
+        <Box className={classes.waitingList}>
             <Container>
                 <Box className={classes.pageHeader}>
                     <Typography variant="h3" className="title">
@@ -32,7 +42,7 @@ const Services = () => {
                     </Breadcrumbs>
                 </Box>
                 <Box className="waiting-list">
-                    <Grid container spacing={4}>
+                    <Grid container spacing={8}>
                         <Grid item xs={12} sm={6}>
                             <Typography color="secondary">
                                 I wasn’t able to find a open session time. Can I get on the CryptoConsultz waiting list?
@@ -66,7 +76,7 @@ const Services = () => {
                             </Typography>
                             <Box component="br" />
                             <Typography color="textSecondary">
-                                Yes. Existing/returning CryptoConsultz clients will have priority on open slots.
+                                Yes. Existing / returning CryptoConsultz clients will have priority on open slots.
                             </Typography>
                             <Box component="br" />
                             <Typography color="secondary">
@@ -81,9 +91,83 @@ const Services = () => {
                                     waitlist@cryptoconsultz.com
                                 </Link>
                             </Typography>
+                            <Box component="br" />
                         </Grid>
                         <Grid item xs={12} sm={6}>
-
+                            <Stack direction="row" justifyContent="space-between" spacing={2}>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    label="First Name"
+                                    color="secondary"
+                                />
+                                <TextField
+                                    required
+                                    fullWidth
+                                    label="Last Name"
+                                    color="secondary"
+                                />
+                            </Stack>
+                            <TextField
+                                required
+                                fullWidth
+                                label="Email"
+                                margin="normal"
+                                color="secondary"
+                            />
+                            <TextField
+                                required
+                                fullWidth
+                                margin="normal"
+                                label="Phone Number"
+                                color="secondary"
+                            />
+                            <Box component="br" />
+                            <FormControl component="fieldset" style={{
+                                marginTop: 32
+                            }}>
+                                <FormLabel component="legend">Choose One *</FormLabel>
+                                <RadioGroup
+                                    aria-label="gender"
+                                    defaultValue="female"
+                                    name="radio-buttons-group"
+                                >
+                                    <FormControlLabel value="15" control={<Radio />} label="15 min consultation" />
+                                    <FormControlLabel value="30" control={<Radio />} label="30 min consultation" />
+                                    <FormControlLabel value="60" control={<Radio />} label="60 min consultation" />
+                                    <FormControlLabel value="other" control={<Radio />} label="I am interested in multi-session packages." />
+                                </RadioGroup>
+                            </FormControl>
+                            <TextField
+                                required
+                                fullWidth
+                                multiline
+                                rows={5}
+                                margin="normal"
+                                label="Please describe what types of services you are looking for."
+                                color="secondary"
+                                style={{
+                                    marginTop: 32
+                                }}
+                            />
+                            <FormGroup
+                                style={{
+                                    marginTop: 16
+                                }}
+                            >
+                                <FormControlLabel control={<Checkbox defaultChecked />} label="I agree to receiving marketing and promotional materials" />
+                            </FormGroup>
+                            <Button
+                                style={{
+                                    marginTop: 16
+                                }}
+                                variant="contained"
+                                color="secondary"
+                                fullWidth
+                                size="large"
+                            >
+                                Schedule
+                            </Button>
                         </Grid>
                     </Grid>
                 </Box>
