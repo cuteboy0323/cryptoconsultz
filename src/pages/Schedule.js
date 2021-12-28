6cf243e46beac4f02fb38c621cee4f4607ac36b5
✨Starbinna✨ <starbinna1004@gmail.com>
2021-12-28 22:50:44 +0900

    Update Schedule.js

--- src/pages/Schedule.js
+++ src/pages/Schedule.js
@@ -1,8 +1,39 @@
 import React from "react";
 
+import Box from "@mui/material/Box";
+import Container from '@mui/material/Container';
+import Typography from "@mui/material/Typography";
+import Breadcrumbs from '@mui/material/Breadcrumbs';
+import Link from '@mui/material/Link';
+
+import useStyles from "../assets/styles";
+
 const Schedule = () => {
-    return(
-        <>123</>
+    const classes = useStyles();
+
+    return (
+        <Box className={classes.schedule}>
+            <Container>
+                <Box className={classes.pageHeader}>
+                    <Typography variant="h3" className="title">
+                        Schedule & Pay
+                    </Typography>
+                    <Breadcrumbs separator="-">
+                        <Link
+                            underline="none"
+                            href="/"
+                        >
+                            Home
+                        </Link>,
+                        <Typography>
+                        Schedule & Pay
+                        </Typography>
+                    </Breadcrumbs>
+                </Box>
+                <Box className="schedule">
+                </Box>
+            </Container>
+        </Box>
     )
 }
 
