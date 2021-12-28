f59d9d0ed87c2882110874bf5537d783f12ee1e0
✨Starbinna✨ <starbinna1004@gmail.com>
2021-12-28 22:50:02 +0900

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,12 +12,14 @@ import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
+import Link from "@mui/material/Link";
 
 import Inventory2RoundedIcon from '@mui/icons-material/Inventory2Rounded';
 import EventNoteRoundedIcon from '@mui/icons-material/EventNoteRounded';
 import LocalPoliceRoundedIcon from '@mui/icons-material/LocalPoliceRounded';
 import GroupsRoundedIcon from '@mui/icons-material/GroupsRounded';
 import PlaylistAddCheckRoundedIcon from '@mui/icons-material/PlaylistAddCheckRounded';
+import QrCodeRoundedIcon from '@mui/icons-material/QrCodeRounded';
 import SupportAgentRoundedIcon from '@mui/icons-material/SupportAgentRounded';
 
 import useStyles from "../assets/styles";
@@ -54,54 +56,76 @@ const Footer = () => {
                             </Grid>
                             <Grid item xs={12} sm={4}>
                                 <List>
-                                    <ListItem disablePadding>
-                                        <ListItemButton>
-                                            <ListItemIcon>
-                                                <Inventory2RoundedIcon />
-                                            </ListItemIcon>
-                                            <ListItemText primary="Sesion Packages & Pricing" />
-                                        </ListItemButton>
-                                    </ListItem>
-                                    <ListItem disablePadding>
-                                        <ListItemButton>
-                                            <ListItemIcon>
-                                                <EventNoteRoundedIcon />
-                                            </ListItemIcon>
-                                            <ListItemText primary="Schedule & Pay" />
-                                        </ListItemButton>
-                                    </ListItem>
-                                    <ListItem disablePadding>
-                                        <ListItemButton>
-                                            <ListItemIcon>
-                                                <PlaylistAddCheckRoundedIcon />
-                                            </ListItemIcon>
-                                            <ListItemText primary="Waiting List" />
-                                        </ListItemButton>
-                                    </ListItem>
-                                    <ListItem disablePadding>
-                                        <ListItemButton>
-                                            <ListItemIcon>
-                                                <SupportAgentRoundedIcon />
-                                            </ListItemIcon>
-                                            <ListItemText primary="Contact Us" />
-                                        </ListItemButton>
-                                    </ListItem>
-                                    <ListItem disablePadding>
-                                        <ListItemButton>
-                                            <ListItemIcon>
-                                                <GroupsRoundedIcon />
-                                            </ListItemIcon>
-                                            <ListItemText primary="Team" />
-                                        </ListItemButton>
-                                    </ListItem>
-                                    <ListItem disablePadding>
-                                        <ListItemButton>
-                                            <ListItemIcon>
-                                                <LocalPoliceRoundedIcon />
-                                            </ListItemIcon>
-                                            <ListItemText primary="Terms of Service Agreement" />
-                                        </ListItemButton>
-                                    </ListItem>
+                                    <Link href="/session-packages-and-pricing" underline="none" color="inherit">
+                                        <ListItem disablePadding>
+                                            <ListItemButton>
+                                                <ListItemIcon>
+                                                    <Inventory2RoundedIcon />
+                                                </ListItemIcon>
+                                                <ListItemText primary="Sesion Packages & Pricing" />
+                                            </ListItemButton>
+                                        </ListItem>
+                                    </Link>
+                                    <Link href="/schedule-and-pay" underline="none" color="inherit">
+                                        <ListItem disablePadding>
+                                            <ListItemButton>
+                                                <ListItemIcon>
+                                                    <EventNoteRoundedIcon />
+                                                </ListItemIcon>
+                                                <ListItemText primary="Schedule & Pay" />
+                                            </ListItemButton>
+                                        </ListItem>
+                                    </Link>
+                                    <Link href="/waiting-list" underline="none" color="inherit">
+                                        <ListItem disablePadding>
+                                            <ListItemButton>
+                                                <ListItemIcon>
+                                                    <PlaylistAddCheckRoundedIcon />
+                                                </ListItemIcon>
+                                                <ListItemText primary="Waiting List" />
+                                            </ListItemButton>
+                                        </ListItem>
+                                    </Link>
+                                    <Link href="/terms-of-service-agreement" underline="none" color="inherit">
+                                        <ListItem disablePadding>
+                                            <ListItemButton>
+                                                <ListItemIcon>
+                                                    <LocalPoliceRoundedIcon />
+                                                </ListItemIcon>
+                                                <ListItemText primary="Terms of Service Agreement" />
+                                            </ListItemButton>
+                                        </ListItem>
+                                    </Link>
+                                    <Link href="/team" underline="none" color="inherit">
+                                        <ListItem disablePadding>
+                                            <ListItemButton>
+                                                <ListItemIcon>
+                                                    <GroupsRoundedIcon />
+                                                </ListItemIcon>
+                                                <ListItemText primary="Team" />
+                                            </ListItemButton>
+                                        </ListItem>
+                                    </Link>
+                                    <Link href="/crypto-products" underline="none" color="inherit">
+                                        <ListItem disablePadding>
+                                            <ListItemButton>
+                                                <ListItemIcon>
+                                                    <QrCodeRoundedIcon />
+                                                </ListItemIcon>
+                                                <ListItemText primary="Crypto Products" />
+                                            </ListItemButton>
+                                        </ListItem>
+                                    </Link>
+                                    <Link href="/contact" underline="none" color="inherit">
+                                        <ListItem disablePadding>
+                                            <ListItemButton>
+                                                <ListItemIcon>
+                                                    <SupportAgentRoundedIcon />
+                                                </ListItemIcon>
+                                                <ListItemText primary="Contact Us" />
+                                            </ListItemButton>
+                                        </ListItem>
+                                    </Link>
                                 </List>
                             </Grid>
                         </Grid>
